d5c5d7baf8cdf33551d90a6043b7ef46ce1bb5cd
Becauseily007 <s66309010008@kktech.ac.th>
2024-07-25 14:31:27 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+        int q,w,total;
+        q = 62;
+        w = 99;
+        total = q+w;
+
+       
+        System.out.println("q + w = "+total);
+    }
+}
