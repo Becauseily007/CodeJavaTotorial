d9a58b1e7d09cb7586561200754439f48c098b4a
Becauseily007 <s66309010008@kktech.ac.th>
2024-08-01 10:07:28 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(" result in forloop : "+i);
+
+        }
+        int iwhile = 1;
+        while (iwhile <= 10){
+            System.out.println("result in whileLoop : "+iwhile);
+            iwhile++ ;
+        }
+    }
+}
