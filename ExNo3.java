f4ed02606d5adfe6072ddadc66b28101a03e8ddb
Becauseily007 <s66309010008@kktech.ac.th>
2024-08-01 14:27:19 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+    int number = 8;
+
+    System.out.println("Multiplication table for " + number + ":");
+
+    for (int k = 1; k <= 12; k++) {
+       
+        System.out.println(number + " x " + k + " = " + (number * k));
+    }
+}
+
