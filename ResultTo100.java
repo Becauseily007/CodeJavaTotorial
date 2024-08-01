56e6441da2e091534f9f37c3ed887c2ed658538b
Becauseily007 <s66309010008@kktech.ac.th>
2024-08-01 10:07:24 +0700

    Create ResultTo100.java

---
+++ ResultTo100.java
@@ -0,0 +1,15 @@
+public class ResultTo100 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int number = 1;
+
+        while (number <= 100) {
+            sum += number;  
+            number += 1;    
+        }
+
+        
+        System.out.println("ผลรวมของจำนวนจาก 1 ถึง 100 คือ: " + sum);
+    }
+}
